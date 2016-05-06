d7c218b41b4b08c5305ef77f59843c18faf81936
Ageir9 <asgeiremil@gmail.com>
2016-05-06 10:18:36 +0000

    TODO: fix shipId to typeName, show attackers, show system name.

--- js/script.js
+++ js/script.js
@@ -16,28 +16,6 @@ new Vue({
         $.getJSON("js/data.json", function (killData) {
             killData.forEach(function (data) {
                 var strippedData = data;
-
-                //Hendir burt óþarfa hlutum
-                /*delete strippedData.killID;
-                delete strippedData.solarSystemID;
-                delete strippedData.moonID;
-                delete strippedData.victim.characterID;
-                delete strippedData.victim.corporationID;
-                delete strippedData.victim.allianceId;
-                delete strippedData.victim.factionID;
-                delete strippedData.victim.factionName;
-                delete strippedData.attackers.characterID;
-                delete strippedData.attackers.corporationID;
-                delete strippedData.attackers.allianceId;
-                delete strippedData.attackers.factionID;
-                delete strippedData.attackers.factionName;
-                delete strippedData.attackers.securityStatus;
-                delete strippedData.attackers.weaponTypeID;
-                delete strippedData.items;
-                delete strippedData.zkb.locationID;
-                delete strippedData.zkb.hash;
-                delete strippedData.zkb.points;*/
-                //return data
             });
             
             $this.$data.output = killData;
