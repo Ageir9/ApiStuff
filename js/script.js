fb1ba57abeb75781857b92340d584a42cd5d87a4
Ageir9 <asgeiremil@gmail.com>
2016-05-09 00:12:41 +0000

    Added routing. Routing breaks it.

--- js/script.js
+++ js/script.js
@@ -3,31 +3,49 @@
 /*global $, jQuery, alert*/
 "use strict";
 Vue.config.debug = true;
+
 //Router
-var Top = Vue.extend({
-    template: '<p>Go to bottom</p>'
-});
-
-var Bottom = Vue.extend({
-    template: '<p>Go to top</p>'
-});
-
-//var Vue = require('vue');
-//var VueRouter = require('vue-router');
+//Býr til router http://router.vuejs.org/en/installation.html
+//Brýtur allt í leiðinni
+var Vue = require('vue');
+var VueRouter = require('vue-router');
 Vue.use(VueRouter);
 
 
+var Top = Vue.extend({
+    template: '<p>Go to bottom</p>'
+}),
+    Bottom = Vue.extend({
+        template: '<p>Go to top</p>'
+    });
+
+var App = Vue.extend({}),
+    router = new VueRouter({});
+//where to go
+router.map({
+    '/top': {
+        component: Top
+    },
+    '/bottom': {
+        component: Bottom
+    }
+});
+//initialize router
+router.start(App, '#app');
+
 new Vue({
-    el: '#nuggets',
+    el: '#app',
     data: {
         loading : true,
         output: {}
     },
     attached: function () {
+        //Þetta virkar einhvernveginn
         var $this = this;
+        
         //https://zkillboard.com/api/alliance/99006112/
-        //https://zkillboard.com/api/character/94406674/
-        $.getJSON("js/chardata.json", function (killData) {
+        //js/chardata.json
+        $.getJSON("https://zkillboard.com/api/character/94406674/", function (killData) {
             killData.forEach(function (data) {
                 var strippedData = data;
             });
