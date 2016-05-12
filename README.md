c1c8bd688ee21959c2c5e2cbeb1f2943afab29cd
Ageir9 <asgeiremil@gmail.com>
2016-05-12 15:27:25 +0000

    no message

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ApiStuff
 Javascript verkefni
+Character killboard, birtir upplýsingar frá zkillboard.com API í töflu og birtir í single-page vef-app.
+
+# To-do
+- Setja inn characterID og skipta þannig um character.
+- Skipta á milli character, corporation, alliance. 
--- js/script.js
+++ js/script.js
@@ -4,23 +4,80 @@
 "use strict";
 Vue.config.debug = true;
 
+//Býr til router
+//http://router.vuejs.org/en/installation.html
+//Brýtur allt í leiðinni
+/*
+var Vue = require('vue');
+var VueRouter = require('vue-router');
+Vue.use(VueRouter);
+
+
+var Top = Vue.extend({
+    template: '<p>Go to bottom</p>'
+}),
+    Bottom = Vue.extend({
+        template: '<p>Go to top</p>'
+    });
+
+var App = Vue.extend({}),
+    router = new VueRouter({});
+//where to go
+router.map({
+    '/top': {
+        component: Top
+    },
+    '/bottom': {
+        component: Bottom
+    }
+});
+//initialize router
+router.start(App, '#app');
+*/
 new Vue({
-    el: '#nuggets',
+    el: '#app',
     data: {
         loading : true,
         output: {}
     },
     attached: function () {
+        //Þetta virkar einhvernveginn
         var $this = this;
-        //https://zkillboard.com/api/alliance/99006112/
-        $.getJSON("js/data.json", function (killData) {
-            killData.forEach(function (data) {
-                var strippedData = data;
+        var shipdata;
+        
+        //js/chardata.json
+        //https://zkillboard.com/api/character/94406674/
+        $.getJSON("https://zkillboard.com/api/alliance/99006112/", function (killData) {
+
+            $.getJSON("js/shipdump.json", function (ships) {
+                shipdata = ships;
+                killData.forEach(function (element, index, array) {
+                    //console.log(element);
+
+                    if (element.attackers.characterName == ""  || element.attackers.corporationName == "CONCORD")
+                    {
+                        element.attackers[0].characterName == "CONCORD Officer";
+                    }
+                    else
+                    {
+                        element.attackers[0].characterName == "Unknown";
+                    }
+                    shipdata.forEach(function (shipElement, shipIndex, shipArray) {
+                        if (shipElement.typeId === element.victim.shipTypeID) {
+                            element.victim.shipTypeID = shipElement.typeName;
+                            return;
+                        }
+                    });
+                });
+                
             });
+            console.log(killData[1]);
             
             $this.$data.output = killData;
             console.log($this.output);
+            //loading fyrir appid
             $this.loading = false;
         });
+        
     }
 });
